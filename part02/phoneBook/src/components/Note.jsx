e1a70638bb07958359f3b20b085ffbd6afc1057b
Rubén Pírez <ruben.pirez.adm@spain.burlingtonbooks.com>
2025-03-17 12:27:42 +0100

    feat-add catch error

--- part02/phoneBook/src/components/Note.jsx
+++ part02/phoneBook/src/components/Note.jsx
@@ -46,6 +46,12 @@ export function Note() {
             .then((returnedNote) => {
                 setNotes(notes.map((note) => (note.id !== id ? note : returnedNote)))
             })
+            .catch((error) => {
+                alert(`the note '${note.content}' was already deleted from server`)
+                setNotes(notes.filter((n) => n.id !== id))
+                console.warn('error', error)
+            }
+        )
         console.log(`Botón presionado de la nota con id ${id}`)
     }
 
